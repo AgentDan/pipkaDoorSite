06609275605ab5ddd02cef4b9ecec2433405c2e3
AgentDan <pregrada@mail.com>
2023-03-26 12:35:58 +0500

    com2

---
+++ client/src/Menu/Menu.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react'
+import {GeneralContainer, Wrapper} from "./Menu.elements"
+import Properties from "../Properties/Properties"
+import Rings from "../Ring/Rings"
+import Futer from "../Futer/Futer"
+
+const Menu = () => {
+    const [currentRotateOne, setCurrentRotateOne] = useState({name: "a1"})
+    const [currentRotateTwo, setCurrentRotateTwo] = useState({name: "a1"})
+    const [currentRotateThree, setCurrentRotateThree] = useState({name: "a1"})
+    const [currentRotateFour, setCurrentRotateFour] = useState({name: "a1"})
+    return (
+        <div>
+            <GeneralContainer>
+                <Wrapper>
+                    <Properties
+                        setCurrentRotateOne={setCurrentRotateOne}
+                        setCurrentRotateTwo={setCurrentRotateTwo}
+                        setCurrentRotateThree={setCurrentRotateThree}
+                        setCurrentRotateFour={setCurrentRotateFour}
+                        currentRotateOne={currentRotateOne}
+                        currentRotateTwo={currentRotateTwo}
+                        currentRotateThree={currentRotateThree}
+                        currentRotateFour={currentRotateFour}
+                    />
+                    <Rings
+                        currentRotateOne={currentRotateOne}
+                        currentRotateTwo={currentRotateTwo}
+                        currentRotateThree={currentRotateThree}
+                        currentRotateFour={currentRotateFour}
+                    />
+                </Wrapper>
+            </GeneralContainer>
+            <Futer/>
+        </div>
+    )
+}
+
+export default Menu
